2aef242e462b44010bebe1356fa0afbd9bc2fdda
amateurenv <dboe241@gmail.com>
2026-07-10 11:38:49 +0300

    shames

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 class BoardCreate(BaseModel):
     url: str = Field(..., min_length=1, max_length=10, description="Короткая ссылка (например 'b')")
@@ -9,3 +10,22 @@ class ThreadCreate(BaseModel):
 
 class PostCreate(BaseModel):
     content: str = Field(..., min_length=1, max_length=2000, description="Текст ответа")
+
+
+class BoardResponse(BaseModel):
+    id: int
+    url: str
+    name: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True 
+
+class ThreadResponse(BaseModel):
+    id: int
+    board_id: int
+    title: str
+    created_at: datetime
+    display_number: int  
+    class Config:
+        from_attributes = True
